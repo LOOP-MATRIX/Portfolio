b6c2a730b441741d0bd8917da657867c9d4db687
learnandsail <fcs090sutaromkar@gmail.com>
2025-07-06 12:36:09 +0530

    meia query  fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -44,7 +44,7 @@ const Projects = () => {
   return (
     <section className="p-4 md:p-6 w-full lg:w-7/9 xl:w-5/9 flex flex-col justify-center items-center">
       <h2 className="text-4xl font-bold mb-8 ">Projects </h2>
-      <div className="space-y-4 ">
+      <div className="w-full space-y-4 ">
         {projects.map((proj, index) => (
           <div key={index} className="border hover:shadow-md shadow-[rgb(77,85,122)] hover:border-[rgb(77,85,122)] border-gray-600 rounded-lg p-4 hover:bg-[#1b1b1b49] flex flex-col gap-8 md:flex-row hover:scale-101 transition-all duration-300 ease-in-out">
             <div className="w-full md:w-[180px] h-[150px] flex-shrink-0 ">
@@ -56,7 +56,7 @@ const Projects = () => {
             </div>
             <div className="ml-4 flex flex-col justify-center gap-2">
               <div className='flex items-center gap-2'>
-                <p className='text-2xl font-semibold'>{proj.title}</p>
+                <p className='text-xl md:text-2xl font-semibold'>{proj.title}</p>
                 {
                   proj.ref&&(
                     <p className='text-md text-blue-400'>({proj.ref})</p>
@@ -64,7 +64,7 @@ const Projects = () => {
                 }
                 <a href={proj.link} target='_blank'><FaExternalLinkAlt fontSize={14}/></a>
                 </div>
-              <div className="text-sm text-white italic flex gap-2 ">{
+              <div className="text-sm text-white italic flex gap-2 flex-wrap">{
                 proj.stack.map((item, index) => (
                   <span key={index} className='px-[8px] text-[12px] text-gray-200 py-[2px] bg-gray-800 rounded-xl'>{item}</span>
                 ))}</div>
